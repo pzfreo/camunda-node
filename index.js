9ffa0ae81c1ff1db59310446fe4e4ba3308140b4
Paul Fremantle <pzfreo@github.com>
2021-04-14 21:00:35 +0100

    .

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'creditScoreChecker'
+client.subscribe("creditScoreChecker", async function({ task, taskService }) {
+  console.log("I just got called");
+  await taskService.complete(task);
+});
